451fcde6852f3da13d70b4b8c227f26544172f45
Andres Rueda <garo99@gmail.com>
2020-08-01 14:06:19 -0400

    exercise 1

---
+++ src/components/Tweet/TweetContext.js
@@ -0,0 +1,27 @@
+import React, { createContext } from "react";
+import avatar from "../../assets/carmen-sandiego.png";
+
+export const TweetContext = createContext(null);
+
+export const TweetProvider = ({ children }) => {
+  const tweetContents = "Where in the world am I?";
+  const displayName = "Carmen Sandiego ✨";
+  const username = "carmen-sandiego";
+  const avatarSrc = avatar;
+  const isRetweetedByCurrentUser = false;
+  const isLikedByCurrentUser = false;
+  return (
+    <TweetContext.Provider
+      value={{
+        tweetContents,
+        displayName,
+        username,
+        avatarSrc,
+        isRetweetedByCurrentUser,
+        isLikedByCurrentUser,
+      }}
+    >
+      {children}
+    </TweetContext.Provider>
+  );
+};
